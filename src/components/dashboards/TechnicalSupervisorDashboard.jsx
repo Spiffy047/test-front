1e380a23101e2b1c5bb2fcb8c1c17a35107000e8
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-10-27 22:00:37 +0300

    Increase max width by 15% to max-w-6xl

--- src/components/dashboards/TechnicalSupervisorDashboard.jsx
+++ src/components/dashboards/TechnicalSupervisorDashboard.jsx
@@ -113,7 +113,7 @@ export default function TechnicalSupervisorDashboard({ user, onLogout }) {
         </div>
       </header>
 
-      <main className="max-w-5xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+      <main className="max-w-6xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <div className="mb-6">
           <input
             type="text"

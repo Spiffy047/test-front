4f85a8dfd734b1952c1460c464533f3009a22db5
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-10-27 21:57:17 +0300

    Reduce max width by 30% to max-w-5xl

--- src/components/dashboards/NormalUserDashboard.jsx
+++ src/components/dashboards/NormalUserDashboard.jsx
@@ -101,7 +101,7 @@ export default function NormalUserDashboard({ user, onLogout }) {
         </div>
       </header>
 
-      <main className="flex-1 px-4 py-8 sm:px-6 lg:px-8">
+      <main className="max-w-5xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <div className="mb-6">
           <input
             type="text"
